f6131eb594f131a1df9dcec179617aa57a149b2f
Alex Mason <Axman6@gmail.com>
2021-06-20 08:45:39 +1000

    Initial baseline param tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,22 @@
 module Main (main) where
 
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Crypto.Number.ModArithmetic
+import Crypto.Number.Prime
+
+import Group
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = defaultMain $ testGroup "ElectionGuard"
+  [ testCase "Baseline Parameters" $ do
+     isProbablyPrime p @? "P is prime"
+     isProbablyPrime q @? "Q is prime"
+     assertEqual "p-1 = qr" (p-1) (q*r)
+     (r `mod` q /= 0) @? "q is not a divisor of r"
+     1 < g @? "1 < g"
+     g < p @? "g < p"
+     expFast g q p == 1 @? "g^q mod p = 1"
+
+  ]
