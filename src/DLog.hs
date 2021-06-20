1b4f4a5f5afad3c304d811f4cb5cb47ba7183c9a
Alex Mason <Axman6@gmail.com>
2021-06-20 15:27:24 +1000

    Implement dlog using MemoTrie, fix g'

--- src/DLog.hs
+++ src/DLog.hs
@@ -1,6 +1,27 @@
 module DLog (module DLog) where
 
-import Group
+import Group ( ElementModP, ElementMod(ElementMod), g, mult )
+import Data.MemoTrie (memo)
+data LogCache = LogCache
+  !ElementModP
+  !Int
+  LogCache
+
+logCache :: LogCache
+logCache = LogCache (ElementMod 1) 0 (go logCache) where
+  go (LogCache g0 n xs) = let n' = n+1 in LogCache (mult [g0,ElementMod g]) n' (go xs)
+
+takeLog :: Int -> LogCache -> [(ElementModP,Int)]
+takeLog = go where
+  go k (LogCache e n xs)
+    | k <= 0 = []
+    | otherwise = (e,n) : go (k-1) xs
+
+dlogFind :: ElementModP -> Int
+dlogFind e = find logCache where
+  find (LogCache e' n xs)
+    | e == e' = n
+    | otherwise = find xs
 
 dlog :: ElementModP -> Int
-dlog e = 0
+dlog = memo dlogFind
