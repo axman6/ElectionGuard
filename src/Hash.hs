2964b21b0d4d404a6440448119384536b60d5a95
Alex Mason <Axman6@gmail.com>
2021-06-23 20:40:52 +1000

    WIP hashing

--- src/Hash.hs
+++ src/Hash.hs
@@ -2,42 +2,93 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Hash (module Hash) where
 
-import Crypto.Number.Serialize (i2osp, os2ip)
+import Crypto.Number.Serialize (os2ip)
 import Crypto.Hash
-import Data.ByteArray ( Bytes, ByteArray )
-import Data.Foldable (foldl')
+    ( hashFinalize, hashInit, hashUpdate, hashUpdates, SHA256, Context, Digest )
+import Data.ByteArray ( ByteArray, convert )
 
-data Input p
-  = External Integer
-  | Proof p
+import Group (ElementModQ, elementMod, toHex, ElementMod, AsInteger (asInteger))
+import Data.ByteString (ByteString)
+import Data.Foldable (toList)
 
-spec :: (p -> Integer) -> Input p -> Integer
-spec f = \case
-  External i -> i
-  Proof p -> f p
+-- data Input p
+--   = External Integer
+--   | Proof p
 
-hashIntegers :: [Integer] -> Integer
-hashIntegers = finaliseInteger . updateAll hashInit
+-- spec :: (p -> Integer) -> Input p -> Integer
+-- spec f = \case
+--   External i -> i
+--   Proof p -> f p
 
-updateAll :: (Foldable t, Hashed a) => Context SHA256 -> t a -> Context SHA256
-updateAll = foldl' hashedUpdate
+-- hashIntegers :: [Integer] -> ElementModQ
+-- hashIntegers = finaliseElement . flip hashComponents initContext
+
+-- updateAll :: (Foldable t, Hashed a) => Context SHA256 -> t a -> Context SHA256
+-- updateAll = foldl' (\acc a -> hashComponents (hashComponents acc a) pipe)
 
 finalise :: Context SHA256 -> Digest SHA256
 finalise = hashFinalize
 
-finaliseInteger :: Context SHA256 -> Integer
-finaliseInteger = os2ip . finalise
+finaliseElement :: Context SHA256 -> ElementModQ
+finaliseElement = elementMod . os2ip . finalise
 
-hash :: Hashed a => a -> Integer
-hash = finaliseInteger . hashedUpdate hashInit
+hash :: Hashed a => a -> ElementModQ
+hash a = finaliseElement $ hashUpdates hashInit $ hashComponents' a
 
+hashDirect :: ByteArray ba => ba -> ElementModQ
+hashDirect ba = finaliseElement $ hashUpdate hashInit ba
+
+hashComponents' :: Hashed a => a -> [ByteString]
+hashComponents' a = interspersePipe (hashComponents a [])
+
+interspersePipe :: [ByteString] -> [ByteString]
+interspersePipe xs = pipe : foldr (\x rest -> x : pipe : rest) [] xs
+
+hashOfElements :: [ByteString] -> ByteString
+hashOfElements xs =
+  toHex
+  $ asInteger
+  $ finaliseElement
+  $ hashUpdates @SHA256 @ByteString hashInit
+  $ interspersePipe xs
+
+-- initContext :: Context SHA256
+-- initContext = hashUpdate hashInit pipe
+
+pipe :: ByteString
+pipe = "|"
 class Hashed a where
-  hashedUpdate :: Context SHA256 -> a -> Context SHA256
+  hashComponents :: a -> [ByteString] -> [ByteString]
 
-instance Hashed Integer where
-  hashedUpdate c = hashUpdate c . i2osp @Bytes
+instance {-# OVERLAPS #-} Hashed Integer where
+  hashComponents i = (toHex i :)
+
+instance Hashed (ElementMod p) where
+  hashComponents e = hashComponents (asInteger e)
 
 instance {-# OVERLAPPABLE #-} ByteArray ba => Hashed ba where
-  hashedUpdate = hashUpdate
+  hashComponents ba = (convert ba :)
+
+instance (Hashed a, Hashed b) => Hashed (a,b) where
+  hashComponents (a,b) = hashComponents a . hashComponents b
+
+instance (Hashed a, Hashed b, Hashed c) => Hashed (a,b,c) where
+  hashComponents (a,b,c) = hashComponents (a,(b,c))
+
+instance {-# OVERLAPS #-} Hashed a => Hashed (Maybe a) where
+  hashComponents Nothing =  ("null" :)
+  hashComponents (Just a) = hashComponents a
+
+instance {-# OVERLAPPABLE #-} (Hashed a, Foldable t) => Hashed  (t a) where
+  hashComponents xs next = hashFoldable xs : next
+
+hashFoldable :: (Foldable t, Hashed a) => t a -> ByteString
+hashFoldable xs =
+  let subHash = toHex
+        $ asInteger
+        $ finaliseElement
+        $ hashUpdates @SHA256 @ByteString hashInit
+        $ interspersePipe (foldr hashComponents [] $ toList xs)
+  in subHash
 
 -- hashMessageWithCommitment ::
